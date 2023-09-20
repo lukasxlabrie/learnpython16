c9839b1e0b30a31a48e9cec75f2f5c264c7c89ca
misosucky <126988962+Misosucky@users.noreply.github.com>
2023-09-19 20:18:06 -0400

    target.write for 3 inputs above

--- ex16.py
+++ ex16.py
@@ -21,3 +21,10 @@ line2 = input("Line 2: ")
 line3 = input("Line 3: ")
 
 print("I'm going to write these to the file")
+
+target.write(line1)
+target.write("\n")
+target.write(line2)
+target.write("\n")
+target.write(line3)
+target.wrtie("\n")
